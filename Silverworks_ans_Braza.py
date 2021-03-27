ea6756fcb9af9f62afaf40528f13c9fe551aed39
Kerr_Braza <33704003+Kerr-Braza@users.noreply.github.com>
2021-03-27 15:02:49 -0500

    Added some truncation formatting, final submit @3

--- Silverworks_ans_Braza.py
+++ Silverworks_ans_Braza.py
@@ -22,35 +22,41 @@ for entry in loans:
 '''
 Focused on readability below, copying the structure in the readme file
 and doing the operations line by line.
+
+Note: I did formatting last and I am running out of time, honestly could not refactor
+some code -> I truncated because the round function, even when specifying 2 decimal places,
+would sometimes only return one decimal
 '''
 
 summary = {
     "LoanAmountSummary": {
-        "Sum": sum(loan_am),
-        "Average": statistics.mean(loan_am),
-        "Median": statistics.median(loan_am),
-        "Minimum": min(loan_am),
-        "Maximum": max(loan_am),
+        "Sum": '{0:.2f}'.format(sum(loan_am)),
+        "Average": '{0:.2f}'.format(statistics.mean(loan_am)),
+        "Median": '{0:.2f}'.format(statistics.median(loan_am)),
+        "Minimum": '{0:.2f}'.format(min(loan_am)),
+        "Maximum": '{0:.2f}'.format(max(loan_am)),
     },
     "SubjectAppraisedAmountSummary": {
-        "Sum": sum(subject_am),
-        "Average": statistics.mean(subject_am),
-        "Median": statistics.median(subject_am),
-        "Minimum": min(subject_am),
-        "Maximum": max(subject_am),
+        "Sum": '{0:.2f}'.format(sum(subject_am)),
+        "Average": '{0:.2f}'.format(statistics.mean(subject_am)),
+        "Median": '{0:.2f}'.format(statistics.median(subject_am)),
+        "Minimum": '{0:.2f}'.format(min(subject_am)),
+        "Maximum": '{0:.2f}'.format(max(subject_am)),
     },
     "InterestRateSummary": {
-        "Sum": sum(interest_rate),
-        "Average": statistics.mean(interest_rate),
-        "Median": statistics.median(interest_rate),
-        "Minimum": min(interest_rate),
-        "Maximum": max(interest_rate),
+        "Sum": '{0:.2f}'.format(sum(interest_rate)),
+        "Average": '{0:.2f}'.format(statistics.mean(interest_rate)),
+        "Median": '{0:.2f}'.format(statistics.median(interest_rate)),
+        "Minimum": '{0:.2f}'.format(min(interest_rate)),
+        "Maximum": '{0:.2f}'.format(max(interest_rate)),
     }
 }
 
-answer_1 = open(
-    "C:/Users/VN MK3/Desktop/Silverworks_challenge/answer_1_Braza.txt", "w", encoding="utf-8")
+# print(summary)
 
-json.dump(summary, answer_1, ensure_ascii=False)
+monthlySummary_Braza = open(
+    "C:/Users/VN MK3/Desktop/Silverworks_challenge/monthlySummary_Braza.json", "w", encoding="utf-8")
 
-answer_1.close()
+json.dump(summary, monthlySummary_Braza, ensure_ascii=False)
+
+monthlySummary_Braza.close()
--- Silverworks_ans_Braza_2.py
+++ Silverworks_ans_Braza_2.py
@@ -12,6 +12,10 @@ to make the state grouping dynamic to the json data, I had to create
 the dictionary here to organize and then calculate by state below
 At least O(2n), linear to shift through, organize, and present by state
 and I used optimal Python built-in functions and stats library for calculations on iterables
+
+Note: I did formatting last and I am running out of time, honestly could not refactor
+some code  -> I truncated because the round function, even when specifying 2 decimal places,
+would sometimes only return one decimal, could not fix in time after multiple testing
 '''
 by_state = dict()
 
@@ -39,33 +43,33 @@ for state in by_state:
 
     by_state_summary[state] = {
         "LoanAmountSummary": {
-            "Sum": sum(current["loan_am"]),
-            "Average": statistics.mean(current["loan_am"]),
-            "Median": statistics.median(current["loan_am"]),
-            "Minimum": min(current["loan_am"]),
-            "Maximum": max(current["loan_am"]),
+            "Sum": '{0:.2f}'.format(sum(current["loan_am"])),
+            "Average": '{0:.2f}'.format(statistics.mean(current["loan_am"])),
+            "Median": '{0:.2f}'.format(statistics.median(current["loan_am"])),
+            "Minimum": '{0:.2f}'.format(min(current["loan_am"])),
+            "Maximum": '{0:.2f}'.format(max(current["loan_am"])),
         },
         "SubjectAppraisedAmountSummary": {
-            "Sum": sum(current["subject_am"]),
-            "Average": statistics.mean(current["subject_am"]),
-            "Median": statistics.median(current["subject_am"]),
-            "Minimum": min(current["subject_am"]),
-            "Maximum": max(current["subject_am"]),
+            "Sum": '{0:.2f}'.format(sum(current["subject_am"])),
+            "Average": '{0:.2f}'.format(statistics.mean(current["subject_am"])),
+            "Median": '{0:.2f}'.format(statistics.median(current["subject_am"])),
+            "Minimum": '{0:.2f}'.format(min(current["subject_am"])),
+            "Maximum": '{0:.2f}'.format(max(current["subject_am"])),
         },
         "InterestRateSummary": {
-            "Sum": sum(current["interest_rate"]),
-            "Average": statistics.mean(current["interest_rate"]),
-            "Median": statistics.median(current["interest_rate"]),
-            "Minimum": min(current["interest_rate"]),
-            "Maximum": max(current["interest_rate"]),
+            "Sum": '{0:.2f}'.format(sum(current["interest_rate"])),
+            "Average": '{0:.2f}'.format(statistics.mean(current["interest_rate"])),
+            "Median": '{0:.2f}'.format(statistics.median(current["interest_rate"])),
+            "Minimum": '{0:.2f}'.format(min(current["interest_rate"])),
+            "Maximum": '{0:.2f}'.format(max(current["interest_rate"])),
         },
     }
 
 # print(by_state_summary)
 
-answer_2 = open(
-    "C:/Users/VN MK3/Desktop/Silverworks_challenge/answer_2_Braza.txt", "w", encoding="utf-8")
+monthlyByState_Braza = open(
+    "C:/Users/VN MK3/Desktop/Silverworks_challenge/monthlyByState_Braza.json", "w", encoding="utf-8")
 
-json.dump(by_state_summary, answer_2, ensure_ascii=False)
+json.dump(by_state_summary, monthlyByState_Braza, ensure_ascii=False)
 
-answer_2.close()
+monthlyByState_Braza.close()
